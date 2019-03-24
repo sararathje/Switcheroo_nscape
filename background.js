aa2a0785f485c6b75ae60c6ae75abf0cd97f2b30
Sara Rathje <sararathje@gmail.com>
2019-03-24 11:36:30 -0600

    Redirecting on request

--- background.js
+++ background.js
@@ -1,3 +1,18 @@
 (function(chrome) {
- // not needed, can remove after
+  chrome.webRequest.onBeforeRequest.addListener(
+    handleRedirect,
+    {urls: ["*://runescape.fandom.com/wiki/*"]},
+    ['blocking']
+  );
+
+  function handleRedirect(details) {
+    const ORIGINAL_ORIGIN = 'runescape.fandom.com/wiki',
+          UPDATED_ORIGIN = 'runescape.wiki/w';
+
+    const updatedUrl = details.url.replace(ORIGINAL_ORIGIN, UPDATED_ORIGIN);
+
+    return {
+      redirectUrl: updatedUrl
+    };
+  }
 })(chrome);
